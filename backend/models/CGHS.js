17ca09bee06bed240bb32ccee071eed9203d3572
Adarsh-chauhan108 <109390485+Adarsh-chauhan108@users.noreply.github.com>
2026-08-18 23:17:31 +0530

    Update CGHS.js

--- backend/models/CGHS.js
+++ backend/models/CGHS.js
@@ -1,6 +1,5 @@
 // models/CGHS.js
 const mongoose = require("mongoose");
-
 const cghsSchema = new mongoose.Schema({
   username: { type: String, required: true },
   email: { type: String, required: true },
